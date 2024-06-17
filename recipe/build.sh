7330f7819759a42230dc6514d239d70fe8f44291
Mark Harfouche <mark.harfouche@gmail.com>
2024-06-16 21:26:30 -0400

    Cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,13 +8,6 @@ if [[ ${target_platform} =~ osx-64.* ]]; then
     export CXXFLAGS="${CXXFLAGS-} -D_LIBCPP_ENABLE_CXX17_REMOVED_FEATURES -Wno-register"
 fi
 
-# Use of clock_gettime requires librt on platforms with glibc < v2.17
-if [[ ${target_platform} =~ .*linux.* ]]; then
-    # we need lindl for dynamic loading on linux
-    # not sure how to submit patches upstsream....
-    export LDFLAGS="-ldl -lrt -Wl,--as-needed"
-fi
-
 cmake ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE="Release"                                          \
     -DCMAKE_PREFIX_PATH=${PREFIX}                                         \
