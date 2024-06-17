356a14c042bc2a19e0ebb20111741660ba706661
Mark Harfouche <mark.harfouche@gmail.com>
2024-06-16 21:46:38 -0400

    Conditionally enable opencl

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,19 @@ if [[ ${target_platform} =~ osx-64.* ]]; then
     export CXXFLAGS="${CXXFLAGS-} -D_LIBCPP_ENABLE_CXX17_REMOVED_FEATURES -Wno-register"
 fi
 
+# Combine both if statements into one
+if [[ ${target_platform} == "linux-ppc64le" || ${target_platform} == "linux-aarch64" ]]; then
+    ENABLE_OPENCL=OFF
+else
+    ENABLE_OPENCL=ON
+fi
+
 cmake ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE="Release"                                          \
     -DCMAKE_PREFIX_PATH=${PREFIX}                                         \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                                      \
     -DENABLE_OPENMP=ON                                                    \
-    -DENABLE_OPENCL=ON                                                    \
+    -DENABLE_OPENCL=${ENABLE_OPENCL}                                      \
     ..
 
 make -j${CPU_COUNT}
